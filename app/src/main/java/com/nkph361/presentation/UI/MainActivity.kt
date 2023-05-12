5345bf19541774f465f668ef0ff6c4b0ee2c803a
Vitaliy <vi19954@gmail.com>
2023-05-12 14:57:38 +0300

    disable night mode

--- app/src/main/java/com/nkph361/presentation/UI/MainActivity.kt
+++ app/src/main/java/com/nkph361/presentation/UI/MainActivity.kt
@@ -2,6 +2,8 @@ package com.nkph361.presentation.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import com.nkph361.R
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -11,6 +13,7 @@ class MainActivity @Inject constructor() : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
